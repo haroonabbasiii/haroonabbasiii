b4408c9f2fd73c961e383eb3d5937a26a1e2154d
haroonabbasiii <abbasiharoon12@gmail.com>
2023-05-28 02:29:35 +0500

    4th commit

--- src/components/components/Navbar.jsx
+++ src/components/components/Navbar.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
-
+import React, {useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
+import {styles} from '../styles';
+import {navLinks} from './constants';
+import { logo, menu,close} from './assets';
 const Navbar = () => {
   return (
     <div>Navbar</div>
